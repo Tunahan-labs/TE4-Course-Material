7961e0b2bc4884db33d76a342d60c99f42ad5e46
Tunahan-labs <tunahankursun369@gmail.com>
2025-09-18 13:47:28 +0200

    Did loops

---
+++ JavaScript/Loops/script.js
@@ -0,0 +1,64 @@
+//!For loop
+/*for (let i = 1; i <= 5; i++) {
+  console.log("Number", i);
+}
+
+//!While loop
+let count = 6;
+
+while (count <= 10) {
+  console.log("Number", count);
+  count++;
+}
+
+//!Do while loop
+let number = 11;
+do {
+  console.log(`The code will run at least ${number} times.`);
+  number++;
+} while (number <= 20);
+
+//!For of loop
+const items = ["Mangos", "Peaches", "Apples", "Pineapples", "Pears", "Guavas"];
+for (const item of items) {
+  console.log(`My favorite fruit is ${item}`);
+}*/
+
+//!Exercise
+
+//!For loop exercise
+/*for (let number = 1; number <= 10; number++) {
+  console.log(`Number ${number}`);
+}
+
+//!While loop exercise
+let count = 10;
+while (count >= 1) {
+  console.log("Number", count);
+  count--;
+}
+
+//!Do while loop exercise
+let sum = null;
+let i = 1;
+do {
+  sum = sum + i;
+  console.log(`The sum is ${sum}`);
+} while (sum < 20);
+
+//!For of loop exercise
+const classmates = [
+  "Manhal",
+  "Sam",
+  "Lucas",
+  "Tunahan",
+  "Erkan",
+  "Albin",
+  "Michell",
+  "Abinash",
+  "Erik",
+  "Nirari",
+];
+for (const student of classmates) {
+  console.log(`My classmate is ${student}`);
+}*/
